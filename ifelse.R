28db410b76f44fc91d7d202f14c61c69664b3aa3
s kohari <skohari@gmail.com>
2016-07-13 17:44:13 -0400

    Update ifelse.R

--- ifelse.R
+++ ifelse.R
@@ -1,4 +1,6 @@
- ifelse(  la == 2, "It's 2",  
+ ifelse(
+  la == 2, "It's 2",  
     ifelse(la == 3, "It's 3!", 
-      ifelse(la == , "Doesn't matter!"))
+      ifelse(la ==4 , "4 Doesn't matter!", "Neither,2,3,4") 
+    )
  )
