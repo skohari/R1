f367bc6afe24fc5e321e8ae932e5e5ad4304cb15
s kohari <skohari@gmail.com>
2016-08-09 13:03:28 -0400

    Update caretFunctionFn.R

--- caretFunctionFn.R
+++ caretFunctionFn.R
@@ -3,14 +3,14 @@ library(caret)
 data(Sonar)
 str(Sonar[, 1:10])
 set.seed(998)
-inTraining <- createDataPartition(Sonar$Class, p = 0.75, list = FALSE) 	#Decision Point
+inTraining <- createDataPartition(Sonar$Class, p = 0.75, list = FALSE) 	# Decision Point
 training <- Sonar[ inTraining,]
 testing  <- Sonar[-inTraining,]
 dset1 = list(training, testing, training); length(dset1)
-targetVar <- 'Class';                                                   # Decicion Point
+targetVar <- 'Class';                                            # Decicion Point
 
 fitControl <- trainControl(## 10-fold CV
-                           method = "repeatedcv", 	                    # Decision Point
+                           method = "repeatedcv", 	             # Decision Point
                            number = 10,
                            ## repeated ten times
                            repeats = 2
@@ -21,7 +21,7 @@ tv <- as.formula(paste(targetVar, "~", "."));
 set.seed(825)
 for(i in 1:length(dset1)) {
  gbmFit1 <- train(as.formula(tv), data = as.data.frame(dset1[i]),
-                 method = "lda", 				                                # Decision Point
+                 method = "lda", 				              # Decision Point
                  trControl = fitControl,
                  verbose = FALSE)
  print(gbmFit1)
