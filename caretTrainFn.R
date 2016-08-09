b09fa7172300e2397031fd4073781bcdb9e07e7b
s kohari <skohari@gmail.com>
2016-08-09 10:25:40 -0400

    Update caretTrainFn.R

--- caretTrainFn.R
+++ caretTrainFn.R
@@ -1,4 +1,13 @@
 
+library(mlbench)
+library(caret)
+data(Sonar)
+str(Sonar[, 1:10])
+set.seed(998)
+inTraining <- createDataPartition(Sonar$Class, p = 0.75, list = FALSE)
+training <- Sonar[ inTraining,]
+testing  <- Sonar[-inTraining,]
+
 targetVar <- eval(quote(Class ~ .)); 
 
 fitControl <- trainControl(## 10-fold CV
