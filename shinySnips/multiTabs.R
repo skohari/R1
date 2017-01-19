ef34dd5a65cec4e62980161ac11fadb5c276f03f
s kohari <skohari@gmail.com>
2017-01-19 14:17:55 -0500

    dynamic number of tabs

---
+++ shinySnips/multiTabs.R
@@ -0,0 +1,21 @@
+## http://stackoverflow.com/questions/19470426/r-shiny-add-tabpanel-to-tabsetpanel-dynamicaly-with-the-use-of-renderui 
+
+library(shiny)
+runApp(list(
+  ui = pageWithSidebar(
+    headerPanel('Dynamic Tabs'),
+    sidebarPanel(
+      numericInput("nTabs", 'No. of Tabs', 5)
+    ),
+    mainPanel(
+      uiOutput('mytabs')  
+    )
+  ),
+  server = function(input, output, session){
+    output$mytabs = renderUI({
+      nTabs = input$nTabs
+      myTabs = lapply(paste('Tab', 1: nTabs), tabPanel)
+      do.call(tabsetPanel, myTabs)
+    })
+  }
+))
